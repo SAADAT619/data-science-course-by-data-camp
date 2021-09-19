a2dc3c6057a4f9f312a12d5a9a31d6b0a56ddb65
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-09-19 06:44:35 +0000

    update and delete in dictionary

--- main.py
+++ main.py
@@ -16,3 +16,19 @@ europe['poland'] = 'warsaw'
 # Print europe
 print(europe)
 #----------------------------
+
+#distionary update and delete item
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+
+# Update capital of germany
+europe['germany'] = 'berlin'
+
+# Remove australia
+del(europe['australia'])
+
+# Print europe
+print(europe)
+#---------------------------------
