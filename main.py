8038c12e92d742f8fdbb09fb758c97a360f25de8
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-09-19 07:51:50 +0000

    again change

--- main.py
+++ main.py
@@ -32,3 +32,24 @@ del(europe['australia'])
 # Print europe
 print(europe)
 #---------------------------------
+
+#creating sub dictionary and add it with the main dictionary
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+
+# Print out the capital of France
+print(europe['france']['capital'])
+
+# Create sub-dictionary data
+data={'population':59.83,'capital':'rome' }
+
+# Add data to europe under key 'italy'
+europe['italy']=data
+
+# Print europe
+print(europe)
+#------------------------------------
