966e7be138484226410e9bfc14f3a3d3afca32a8
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-09-22 16:34:13 +0000

    new changes

--- new.py
+++ new.py
@@ -31,4 +31,20 @@ print(cars.iloc[5, 2])
 
 # Print sub-DataFrame
 print(cars.loc[['RU', 'MOR'], ['country', 'drives_right']])
+------------------------------------------
+#Another example of loc and iloc
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out drives_right column as Series
+print(cars.loc[:,'drives_right'])
+
+# Print out drives_right column as DataFrame
+print(cars.iloc[:,[2]])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.loc[:,['cars_per_cap','drives_right']])
+
+
 """
