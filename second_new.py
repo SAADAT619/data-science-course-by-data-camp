cf108803f100bdc95f702d1ac6663fa608c4ee70
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-09-22 22:07:03 +0000

    again changes

--- second_new.py
+++ second_new.py
@@ -11,4 +11,15 @@ sel= cars[dr]
 
 # Print sel
 print(sel)
+--------------------------------------
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Convert code to a one-liner
+dr = cars['drives_right']
+sel = cars[dr]
+
+# Print sel
+print(sel)
 """
