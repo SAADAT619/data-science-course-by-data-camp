7b80363546378ae91bcbdb4d9bcfac2a5f7f0ca6
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-09-22 21:06:25 +0000

    if else

--- new.py
+++ new.py
@@ -70,4 +70,26 @@ print(my_house>=18)
 
 # my_house less than your_house
 print(my_house<your_house)
+
+-------------------------------------------
+# Define variables
+room = "bed"
+area = 14.0
+
+# if-elif-else construct for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+elif room == "bed":
+    print("looking around in the bedroom.")
+else :
+    print("looking around elsewhere.")
+
+# if-elif-else construct for area
+if area > 15 :
+    print("big place!")
+elif area > 10:
+    print("medium size, nice!")
+else :
+    print("pretty small.")
+
 """
