1c36cf77110596de663523464900862c9fd2d142
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-09-25 06:41:13 +0000

    again changes

--- loop.py
+++ loop.py
@@ -109,5 +109,14 @@ for lab, row in cars.iterrows() :
     
 # Print cars
 print(cars)
+-------------------------------------------
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Use .apply(str.upper)
+cars["COUNTRY"] = cars["country"].apply(str.upper)
+
+print(cars)
 
 """
