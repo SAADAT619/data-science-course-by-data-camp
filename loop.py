fedc087614483f225a85ffa385b28c538250e2d5
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-09-25 00:39:07 +0000

    while loop

---
+++ loop.py
@@ -0,0 +1,12 @@
+"""
+#while_loop
+# Initialize offset
+offset=8
+
+# Code the while loop
+while offset != 0:
+    print("correcting...")
+    offset = offset - 1
+    print(offset)
+-----------------------------------------
+"""
