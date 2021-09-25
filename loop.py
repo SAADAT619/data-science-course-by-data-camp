c95f8c7747e1c0546cc9af76d0c0fbe6777ef0fd
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-09-25 00:44:03 +0000

    new changes

--- loop.py
+++ loop.py
@@ -9,4 +9,18 @@ while offset != 0:
     offset = offset - 1
     print(offset)
 -----------------------------------------
+
+#while loop with if else statemnet
+# Initialize offset
+offset = -6
+
+# Code the while loop
+while offset != 0 :
+    print("correcting...")
+    if offset>0:
+      offset = offset - 1
+    else : 
+      offset = offset + 1    
+    print(offset)
+------------------------------------------
 """
