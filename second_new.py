b0390c1b0a96f1c9eaf67fe5ecfa3363c031407f
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-09-25 00:13:48 +0000

    new changes again

--- second_new.py
+++ second_new.py
@@ -23,4 +23,16 @@ sel = cars[dr]
 # Print sel
 print(sel)
 print(sel)
+------------------------------------------
+
+#how to write the previous code in one line
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Convert code to a one-liner
+sel = cars[cars['drives_right']]
+
+# Print sel
+print(sel)
 """
