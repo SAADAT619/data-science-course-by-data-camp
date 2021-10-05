85e54eba9588812f7cef2df13f32a61d53001696
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-10-05 10:29:25 +0000

    panda data manipulation

--- second_new.py
+++ second_new.py
@@ -65,5 +65,18 @@ medium=cars[between]
 # Print medium
 print(medium)
 ------------------------------------------
+#working principal of head,info,shape,describe meathod
 
+# Print the head of the homelessness data
+print(homelessness.head())
+
+# Print information about homelessness
+print(homelessness.info())
+
+# Print the shape of homelessness
+print(homelessness.shape)
+
+# Print a description of homelessness
+print(homelessness.describe())
+-------------------------------------------
 """
