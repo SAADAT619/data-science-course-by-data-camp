74ddc6812c53355ec9c01e1b40e4fd9c6614e0b3
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-10-07 09:40:50 +0000

    again some changes

--- main.py
+++ main.py
@@ -189,4 +189,13 @@ south_mid_atlantic = homelessness[(homelessness["region"] == "South Atlantic") |
 # See the result
 print(south_mid_atlantic)
 -------------------------------------------
+# The Mojave Desert states
+canu = ["California", "Arizona", "Nevada", "Utah"]
+
+# Filter for rows in the Mojave Desert states
+mojave_homelessness = homelessness[homelessness["state"].isin(canu)]
+
+# See the result
+print(mojave_homelessness)
+-------------------------------------------
 """
