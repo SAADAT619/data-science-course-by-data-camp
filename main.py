85ed9306cbd33c4fed815cae3e20d71f8a4475df
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-10-07 06:56:33 +0000

    again subset rows

--- main.py
+++ main.py
@@ -169,4 +169,10 @@ ind_gt_10k = homelessness[homelessness["individuals"]>10000]
 # See the result
 print(ind_gt_10k)
 ------------------------------------------
+# Filter for rows where region is Mountain
+mountain_reg = homelessness[homelessness["region"] == "Mountain"]
+
+# See the result
+print(mountain_reg)
+------------------------------------------
 """
