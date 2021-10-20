6635d17b2cdd165e9faf084058c5c932b46013f8
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-10-20 22:12:31 +0000

    adding margins

--- summary_statistics.py
+++ summary_statistics.py
@@ -162,4 +162,7 @@ print(mean_sales_by_type_holiday)
 # Print mean weekly_sales by department and type; fill missing values with 0
 print(sales.pivot_table(values="weekly_sales", index="type", columns="department",fill_value=0))
 -----------------------------------------
+# Print the mean weekly_sales by department and type; fill missing values with 0s; sum all rows and cols
+print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0, margins=True))
+-------------------------------------------
 """
