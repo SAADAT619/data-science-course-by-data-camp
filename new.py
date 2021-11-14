ecf68383557c4aea1da69fa50804e743ac0b3d98
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-11-14 10:36:26 +0000

    new changes

--- new.py
+++ new.py
@@ -106,5 +106,16 @@ sel= cars[dr]
 # Print sel
 print(sel)
 -------------------------------------------
+# Sort the index of temperatures_ind
+temperatures_srt = temperatures_ind.sort_index()
 
+# Subset rows from Pakistan to Russia
+print(temperatures_srt.loc["Pakistan":"Russia"])
+
+# Try to subset rows from Lahore to Moscow
+print(temperatures_srt.loc["Lahore":"Moscow"])
+
+# Subset rows from Pakistan, Lahore to Russia, Moscow
+print(temperatures_srt.loc[("Pakistan", "Lahore"):("Russia", "Moscow")])
+------------------------------------------
 """
---
+++ rtr.py
@@ -0,0 +1,6 @@
+class Linkelist:
+        def __init__(self):
+                self.head = None
+                tail = None
+                for i in a:
+                        n = node.Node(i,None)
