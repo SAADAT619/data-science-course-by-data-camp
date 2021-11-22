911d2ef22b3281fc0f80017ea65e4cfdaaa3095d
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2021-11-22 08:17:06 +0000

    new slicing

--- main.py
+++ main.py
@@ -222,4 +222,13 @@ result = high_homelessness_srt[["state", "indiv_per_10k"]]
 # See the result
 print(result)
 ------------------------------------------
+# Subset rows from India, Hyderabad to Iraq, Baghdad
+print(temperatures_srt.loc[("India","Hyderabad"):("Iraq","Baghdad")])
+
+# Subset columns from date to avg_temp_c
+print(temperatures_srt.loc[:,"date":"avg_temp_c"])
+
+# Subset in both directions at once
+print(temperatures_srt.loc[("India","Hyderabad"):("Iraq","Baghdad"),"date":"avg_temp_c"])
+-------------------------------------------
 """
